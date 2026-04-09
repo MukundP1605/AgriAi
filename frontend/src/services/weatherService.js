ace24ae53aee684a85db77c202038515446636fd
Mukundpatil-cd <mukundp2005@gmail.com>
2026-04-09 16:10:21 +0530

    style: Improve UI/UX for mobile devices

--- frontend/src/services/weatherService.js
+++ frontend/src/services/weatherService.js
@@ -397,3 +397,4 @@ export default new WeatherService();# Updated 2026-07-13 19:39:24
 # Updated 2026-07-13 19:40:25
 # Updated 2026-07-13 21:53:26
 # Updated 2026-07-13 21:54:15
+# Updated 2026-07-13 21:54:22
